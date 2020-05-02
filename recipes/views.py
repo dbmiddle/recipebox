0dc7e6911a7a865ad154332ecd9762b82d842b6e
Mike A <jimalik2525@gmail.com>
2020-05-01 20:51:18 -0400

    So close, author recipes not being fully filtered...

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from recipes.models import Recipe, Author
@@ -22,3 +22,8 @@ class RecipeDetailView(DetailView):
 class AuthorDetailView(DetailView):
     model = Author
     context_object_name = 'author'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        context['personal_recipe_list'] = Recipe.objects.all()
+        return context
